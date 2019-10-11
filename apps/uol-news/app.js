2a456d9fff85f0f0b3418a3a6573c0c13976dcfc
apereira <ademirspr@live.com>
2019-10-11 14:01:55 -0300

    Layout adjust horizontal

--- apps/uol-news/app.js
+++ apps/uol-news/app.js
@@ -6,12 +6,32 @@ import {feedColors, getLinkCode} from './utils';
 import uolLogo from './images/uolLogo.png';
 import adMooHLogo from './images/adMoohLogo.png';
 
+import posed from "react-pose";
+
+const Box = posed.div({
+  hidden: {
+    opacity: 0,
+    width: 0,
+    height: 0
+  },
+  visible: {
+    opacity: 1,
+    width: "100%",
+    height: "100%"
+  }
+});
+
 export default class UolNews extends AppComponent {
   constructor(props) {
     super(props);
     const feedType = props.data.feedType;
+    this.increaseBarInterval = null;
+
+    console.log('Get feed items', feedType);
 
     props.context.getData('feedItems_' + feedType).then(feed => {
+      console.log('Feed items', feed);
+
       const items = JSON.parse(feed);
       const news = this._getNewsWithThumbnail(items);
       if(news !== undefined) {
@@ -26,30 +46,54 @@ export default class UolNews extends AppComponent {
     this.state = {
       feedType,
       news: undefined,
-      rollUp: false,
-      color: feedColors[feedType]
+      color: feedColors[feedType],
+      showSplash: false,
+      barWidth: 10
     };
   }
 
   willShow(){
-		this.setState({rollUp: !this.state.rollUp});
-	}
+    this.setState({showSplash: false});
+    this.increaseBarInterval  = setInterval(this.increaseBar.bind(this), 100);
+
+  }
+
+  increaseBar(){
+    const { barWidth } = this.state;
+    const nbarWidth = barWidth + 1;
+    if(nbarWidth >= 95)
+    {
+      clearInterval(this.increaseBarInterval);
+    }
+    else
+    {
+      this.setState({barWidth: nbarWidth});
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.increaseBarInterval);
+  }
 
   render() {
 
-    const { news, color } = this.state;
-
+    const { news, color, showSplash, barWidth } = this.state;
     if (news === undefined) {
-      return <>{this.state.feedType}</>;
+      return <div>{this.state.feedType}</div>;
     } else {
 
       const type = news.title;
       const description = news.description;
       const picture = news.linkfoto;
 
+
       return (
-          <div style={{ ...layout.template_uol_view }}>
-             <div style={{ ...layout.content, backgroundImage: `url(${picture})` }}>
+        <>
+          <Box key="splash" style={layout.splash} className="box" pose={showSplash ? 'visible' : 'hidden'} >
+            <img style={layout.splash___img} src={uolLogo} />
+          </Box>
+          <div key="news" style={{ ...layout.template_uol_view }} hidden={showSplash}>
+            <div style={{ ...layout.content, backgroundImage: 'url('+picture+')' }}>
               <header style={layout.header_bar}>
                 <div style={layout.header_bar__logo_uol}>
                   <img style={layout.header_bar__logo_uol___img} src={uolLogo} />
@@ -61,7 +105,7 @@ export default class UolNews extends AppComponent {
                 </div>
               </header>
               <div style={{...layout.new, backgroundImage: "linear-gradient(to top, rgba("+ color +", 0.5) 20%, rgba(255, 0, 0, 0) 70%)"}}>
-                <div style={{...layout.new__bar,  backgroundColor: "rgb("+color+")"}}></div>
+                <div style={{...layout.new__bar, width: barWidth+"%" ,backgroundColor: "rgb("+color+")"}}></div>
                 <div style={layout.new__text}>{description}</div>
               </div>
               <footer>
@@ -69,8 +113,9 @@ export default class UolNews extends AppComponent {
                   <img style={layout.footer_admooh___img} src={adMooHLogo} />
                 </div>
               </footer>
-             </div>
+            </div>
           </div>
+        </>
       );
     }
   }
